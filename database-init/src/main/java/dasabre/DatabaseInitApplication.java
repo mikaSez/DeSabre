72972b13e0d389101a74beda62f7895f795c2bc1
MikaSez <mis.strator@gmail.com>
2015-05-16 13:24:08 +0200

    Initialisation and modules.

---
+++ database-init/src/main/java/dasabre/DatabaseInitApplication.java
@@ -0,0 +1,12 @@
+package dasabre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseInitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseInitApplication.class, args);
+    }
+}
---
+++ database-models/src/test/java/desabre/DatabaseModelsApplicationTests.java
@@ -0,0 +1,16 @@
+package desabre;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DatabaseModelsApplication.class)
+public class DatabaseModelsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
