c89466675a81f9a1d289cd1a0dbea3615a8e81d8
MikaSez <mis.strator@gmail.com>
2015-06-11 21:12:30 +0200

    Corrections

--- Application/src/main/java/info/desabre/application/rooting/MvcConfig.java
+++ Application/src/main/java/info/desabre/application/rooting/MvcConfig.java
@@ -14,29 +14,41 @@ public class MvcConfig extends WebMvcConfigurerAdapter implements EmbeddedServle
 
 
     /**
-     * Indicate here the correspondaces between the view and the code.
+     * Indicate here the correspondences between the view and the code.
      * The view controller is the intercepted path, which must be handled in controllers
      * The view name is the template file name who will be handled by this controller.
-     * The view is typicaly under ressources templates and is *.html
+     * The view is typically under resources templates and is *.html
      * The view name is stripped of .html when written here.
      */
 	
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
+        userRegistry(registry);
+        jobRegistry(registry);
+        registry.addViewController("/server/list").setViewName("server/serverList");
+        registry.addViewController("/notification/list").setViewName("notification/notificationList");
+
+
+    }
+
+
+    private void userRegistry(ViewControllerRegistry registry) {
         registry.addViewController("/user/home").setViewName("user/home");
         registry.addViewController("/").setViewName("user/home");
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/admin/home").setViewName("admin/home");
-        registry.addViewController("/job/list").setViewName("job/jobList");
-        registry.addViewController("/job/create").setViewName("job/jobCreate");
-        registry.addViewController("/server/list").setViewName("server/serverList");
-        registry.addViewController("/notification/list").setViewName("notification/notificationList");
         registry.addViewController("/profil").setViewName("user/profil");
-
+        registry.addViewController("/inscription").setViewName("user/inscription");
     }
 
-	@Override
-	public void customize(ConfigurableEmbeddedServletContainer container) {
+
+    private void jobRegistry(ViewControllerRegistry registry) {
+        registry.addViewController("/job/list").setViewName("job/jobList");
+        registry.addViewController("/job/create").setViewName("job/jobCreate");
+    }
+
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
 		container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/error/404"));
 		container.addErrorPages(new ErrorPage(HttpStatus.FORBIDDEN, "/error/403"));
 	}
