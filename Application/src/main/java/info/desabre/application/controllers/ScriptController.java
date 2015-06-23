935f93af543b51047958bb013cddc94445d38b4f
MikaSez <mis.strator@gmail.com>
2015-06-23 20:44:39 +0200

    Ajout d'une vue grille pour les scripts

---
+++ Application/src/main/java/info/desabre/application/controllers/ScriptController.java
@@ -0,0 +1,46 @@
+package info.desabre.application.controllers;
+
+import info.desabre.application.views.grid.ScriptGridView;
+import info.desabre.database.models.job.Script;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * Created by Naiirod on 07/06/2015.
+ * Controleur responsable de la gestion des serveurs.
+ */
+@Controller
+@RequestMapping("/script/**")
+public class ScriptController {
+
+    private static final Logger log = Logger.getLogger(ServerController.class.getName());
+
+    @RequestMapping("list")
+    public String list(Model model) {
+
+        return "script/scriptList";
+    }
+
+    @RequestMapping("/create")
+    public String create(Model model) {
+
+        return "server/serverCreate";
+    }
+
+    @RequestMapping("data")
+    public
+    @ResponseBody
+    List<ScriptGridView> data(Model model) {
+        log.info("data requested");
+        List<Script> scripts = new ArrayList();
+        return Collections.unmodifiableList(ScriptGridView.map(scripts));
+    }
+
+}
