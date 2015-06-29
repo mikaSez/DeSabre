24effc7d5948ea7ed36eed16dcae1f30958b9686
Dorian DAVI <dorian.davi@gmail.com>
2015-06-29 11:29:14 +0200

    Application simple de serveur distant
    C’est trop stylé vous verrez mais on ne peut pas recul l’objet job donc
    pas deserialiser

---
+++ serveur-distant-application/src/info/desabre/Application.java
@@ -0,0 +1,12 @@
+package info.desabre;
+import info.desabre.view.Window;
+
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Window ihm = new Window();
+	}
+
+}
---
+++ serveur-distant-application/src/info/desabre/view/Window.java
@@ -0,0 +1,84 @@
+package info.desabre.view;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+@SuppressWarnings("serial")
+public class Window  extends JFrame {
+	  private JPanel container = new JPanel();
+	  private JTextField jtf = new JTextField("");
+
+	  public Window(){
+	    this.setTitle("Serveur distant - DaSabre");
+	    this.setSize(600, 600);
+	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    this.setLocationRelativeTo(null);
+	    container.setBackground(Color.white);
+	    container.setLayout(new BorderLayout());
+	    JPanel top = new JPanel();
+	    Font police = new Font("Arial", Font.BOLD, 14);
+	    jtf.setFont(police);
+	    jtf.setPreferredSize(new Dimension(600, 600));
+	    jtf.setForeground(Color.BLACK);
+	    jtf.setText(jobsToString());
+	    top.add(jtf);
+	    container.add(top, BorderLayout.NORTH);
+	    this.setContentPane(container);
+	    this.setVisible(true);            
+	  }
+	  
+	  public String jobsToString(){
+		  
+		String directory = "../../../GitHub/DeSaBre/Application/distant";
+		ObjectInputStream ois = null;
+		File file = new File(directory);
+		int i;
+		String texte = "";
+	    	
+		try {
+			if(file.isDirectory()){
+				final String[] files = file.list();
+				
+				for(i=0;i<files.length;i++){
+
+					System.out.println(files[i]);
+					final FileInputStream fichier = new FileInputStream(directory+"/"+files[i]);
+					ois = new ObjectInputStream(fichier);
+//					final Job job = (Job) ois.readObject();
+					
+//					texte += "Job"+i;
+//					texte += "\nId"+job.getId();
+//					texte += "\nNom"+job.getName();
+//					texte += "\nDate"+job.getDate();
+//					texte += "\n";
+//					texte += "\n-------------------------------\n";
+					
+				}
+				
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+		  try {
+		    if (ois != null) {
+		    	ois.close();
+		    }
+		  } catch (final IOException ex) {
+		    ex.printStackTrace();
+		  }
+		}
+		  
+		return texte;
+	  }
+	}
